5ef89a7f672611251d934cfa2a3b5ea99fa13a8a
sq19 <shqi@ucsd.edu>
2021-05-13 22:35:41 -0700

    part 2a

--- sw.js
+++ sw.js
@@ -4,3 +4,48 @@
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    "https://cse110lab6.herokuapp.com/entries"
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+  });
